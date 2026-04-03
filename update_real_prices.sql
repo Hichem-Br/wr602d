7e065fd577ffd6142e6535ad3df53ed615fcd4c2
Hichem-Br <hichem.bnbr@gmail.com>
2026-04-03 20:43:38 +0200

    chore: remove junk files from main

--- update_real_prices.sql
+++
@@ -1,2 +0,0 @@
-UPDATE plan SET stripe_price_id = 'price_1TFcQHFxdzvD6acBte5MWX7h' WHERE name = 'Premium';
-UPDATE plan SET stripe_price_id = 'price_1TFcQtFxdzvD6acBIntTAIyp' WHERE name = 'Enterprise';

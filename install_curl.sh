8a673e4165ff8906fe4b8c41c770324f57259e73
Hichem-Br <hichem.bnbr@gmail.com>
2026-04-03 20:44:16 +0200

    chore: remove junk files from feature/stripe

--- install_curl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/hichem/wr602d
-docker compose exec web bash -c "wget -O /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg && apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y php8.2-curl php-curl && service apache2 reload"
